9df6aace85f4b7661e2d46076ce30e75b649e879
sahidularif <sahidularif1@gmail.com>
2023-01-30 15:28:22 +0600

    update

--- database.js
+++ database.js
@@ -2,10 +2,10 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const DBLOCAL = `mongodb://localhost:27017/powerhack`
-const DB = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.or4h7.mongodb.net/renterbd?retryWrites=true&w=majority`
+const DB = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.or4h7.mongodb.net/powerhack?retryWrites=true&w=majority`
 module.exports = async () => {
     try {
-        await mongoose.connect(DBLOCAL, {
+        await mongoose.connect(DB, {
             autoIndex: false, // Don't build indexes
             maxPoolSize: 10, // Maintain up to 10 socket connections
             serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
